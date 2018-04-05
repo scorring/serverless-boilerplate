b15cabee1876c3269134a4c04e63d63f64809b57
Felix <felix@skont.io>
2018-04-05 16:13:15 +0200

    Update basepath in demo

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
     entry: slsw.lib.entries,
+    mode: "production",
     externals: [nodeExternals()],
     target: 'node',
     resolve: {
