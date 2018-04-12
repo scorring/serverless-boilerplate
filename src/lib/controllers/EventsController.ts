ab1285d07b72795c401960ed952ce881a20d1f1f
Felix <felix@skont.io>
2018-04-12 16:31:36 +0200

    Upgrade engine to 8.10. Add mocha for testing. Add example for Invoking another Lambda Function

--- src/lib/controllers/EventsController.ts
+++ src/lib/controllers/EventsController.ts
@@ -1,6 +1,7 @@
 import {BaseController} from "./BaseController";
 import {HttpRequest} from "../models/http/HttpRequest";
 import {HttpResponse} from "../models/http/HttpResponse";
+import * as AWS from "aws-sdk";
 
 export class EventsController extends BaseController {
     constructor() {
@@ -17,6 +18,27 @@ export class EventsController extends BaseController {
             response.headers['X-Request-ID'] = 4242;
             resolve(response)
         })
+    }
+
+    relist(request: HttpRequest, response: HttpResponse): any {
+        return new Promise((resolve, reject) => {
+            const lambda = new AWS.Lambda();
+            lambda.invoke({FunctionName: process.env['LIST_ARN']}, (err, data) => {
+                console.log(err);
+                console.log(data);
+                if (!err) {
+                    response.statusCode = 200;
+                    response.body = data;
+                    response.headers['X-Request-ID'] = 4242;
+                    resolve(response);
+                } else {
+                    response.statusCode = 400;
+                    response.body = err;
+                    response.headers['X-Request-ID'] = 4243;
+                    resolve(err);
+                }
+            });
+        });
     }
 
     show(request: HttpRequest, response: HttpResponse): any {
