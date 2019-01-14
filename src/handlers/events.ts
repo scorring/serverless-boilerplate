927c56f9c752f5199a261fafe882cde1bade0ad6
Stuart Corring <stuart.corring@gmail.com>
2019-01-14 10:21:40 +0100

    Fix typo on retrieve handler name

--- src/handlers/events.ts
+++ src/handlers/events.ts
@@ -2,4 +2,4 @@ import {EventsController} from "../lib/controllers/EventsController"
 import {BuildHandler} from "../lib/utils/http/RequestHandler"
 
 export const save = BuildHandler(EventsController, "index");
-export const retriev = BuildHandler(EventsController, "show");
+export const retrieve = BuildHandler(EventsController, "show");
